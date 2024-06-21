50239c125f5d86cbddab6b608145b5519478b380
Crollexx <crollexx@gmail.com>
2024-06-21 13:59:42 +0300

    complite class FlashModule

--- Module_flash_cards.js
+++ Module_flash_cards.js
@@ -1,3 +1,23 @@
-class FlashModule{
-    
+const fs = require('fs');
+
+class FlashModule {
+
+    constructor(question, answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    static loadQuestions(filePath) {
+        const data = fs.readFileSync(filePath, 'utf-8');
+        const lines = data.split('\n').filter(line => line.trim() !== '');
+        const questions = [];
+        for (let i = 0; i < lines.length; i += 2) {
+            questions.push(new FlashModule(lines[i], lines[i + 1]));
+        }
+        return questions;
+    }
 }
+
+
+
+module.exports = FlashModule;
