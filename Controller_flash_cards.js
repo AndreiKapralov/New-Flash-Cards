052f382ebb3eed8492548b84fba87222194046ea
unknown <benya0192@mail.ru>
2024-06-21 14:38:05 +0300

    connect modules into Controller

--- Controller_flash_cards.js
+++ Controller_flash_cards.js
@@ -1,5 +1,31 @@
-const inquirer = require('inquirer');
+const Question = require('./Question');
 
 class Controller {
+    constructor() {
+        this.questions = [];
+    }
 
+    loadQuestionsFromFiles(filePaths) {
+        filePaths.forEach(filePath => {
+            this.questions.push(...Question.loadQuestions(filePath));
+        });
+    }
+
+    getQuestion(index) {
+        if (index < this.questions.length) {
+            return this.questions[index];
+        } else {
+            return null;
+        }
+    }
+
+    checkAnswer(index, answer) {
+        if (index < this.questions.length) {
+            return this.questions[index].answer.trim().toLowerCase() === answer.trim().toLowerCase();
+        } else {
+            return false;
+        }
+    }
 }
+
+module.exports = Controller;
